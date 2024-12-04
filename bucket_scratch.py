7d6e37a97ae0783d7a572f540400ae80facd90b5
Bovet Audrey <audrey.bovet@student-cs.fr>
2024-12-04 19:34:26 +0100

    encore sur  bucket

--- bucket_scratch.py
+++ bucket_scratch.py
@@ -1,6 +1,6 @@
 import s3fs
 
-fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://datalab.sspcloud.fr/my-files"})
+fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
 
 MY_BUCKET = "abovet"
 fs.ls(MY_BUCKET)
