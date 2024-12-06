63352d1e713386f002f04cb18a1eba600bed9072
esam <eve.samani@ensae.fr>
2024-12-06 18:08:56 +0000

    bruh time

--- exploration_puffins.py
+++ exploration_puffins.py
@@ -1,33 +1,25 @@
 import pandas as pd
 import s3fs
+import pyarrow as pa
 
-# Configuration des identifiants AWS
+# Tentative de passer par le MinIO Client mais échec total jsp pourquoi
 fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
-  # anon=False pour utiliser vos identifiants configurés
-
 MY_BUCKET = "esam"
-FILE_PATH_S3 = f"s3://{MY_BUCKET}/diffusion/eBirdSep2024.parquet"
+eBird_path = f"s3://{MY_BUCKET}/diffusion/eBird/ebd_atlpuf_relSep-2024.txt"
+fs.download("diffusion/eBird/ebd_atlpuf_relSep-2024.txt", "test.txt")
 
 
 
-# Tester l'accès au bucket
-print(fs.ls("s3://esam/diffusion/"))
 
-# Tester si le fichier existe
-print(fs.exists(FILE_PATH_S3))
-print(fs.exists(FILE_PATH_S3))
-
-
-fs.get(FILE_PATH_S3, "eBirdSep2024.parquet")  # Télécharge le fichier localement
-info = fs.info(FILE_PATH_S3)
-print(info)
+# J'ai pété mon crâne et je passe en local 
+df = pd.read_csv("/home/onyxia/projet_python_2024_ENSAE/ebd_atlpuf_relSep-2024/ebd_atlpuf_relSep-2024.txt", sep="\t", low_memory=False)
+print(df.head())
+df.columns
 
 
 
 
 
-# Lecture directe avec pandas
-eBirdSep2024 = pd.read_parquet(FILE_PATH_S3, filesystem=fs)
 
-# Vérification
-print(eBirdSep2024.head(2))
+
+
