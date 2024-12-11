bfb7d55ff0f8574788526162462fd2e62df351f2
abovet <audrey.bovet@ensae.fr>
2024-12-11 16:11:14 +0000

    j'ai mis en place l'api pour récupérer les données copernicus

---
+++ API_copernicus.py
@@ -0,0 +1,14 @@
+import copernicusmarine
+
+request_dataframe = copernicusmarine.subset(
+  dataset_id="cmems_obs_si_arc_phy_my_L4-DMIOI_P1D-m",
+  variables=["analysed_st", "analysis_error", "mask", "observation_max", "observation_min", "observation_num", "observation_std", "sea_ice_fraction"],
+  minimum_longitude=-179.97500610351562,
+  maximum_longitude=179.97500610351562,
+  minimum_latitude=58,
+  maximum_latitude=89.94999694824219,
+  start_datetime="1983-12-31T00:00:00",
+  end_datetime="2023-12-31T00:00:00",
+)
+
+print(request_dataframe)
