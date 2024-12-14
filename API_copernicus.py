601d23372f76b539570191e4a3bcda3357bbaba0
esam <eve.samani@ensae.fr>
2024-12-14 21:01:21 +0000

    SSP 5

--- API_copernicus.py
+++ API_copernicus.py
@@ -65,23 +65,6 @@ print(fs.ls(MY_BUCKET))
 minio_path = f'{MY_BUCKET}/diffusion/cds_data/tos_Omon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_20000116-20140616.nc'
 
 
-# Télécharger directement les données sur MiniO
-dataset = "projections-cmip6"
-request = {
-    "temporal_resolution": "monthly",
-    "experiment": "historical",
-    "variable": "sea_surface_temperature",
-    "model": "mpi_esm1_2_hr",
-    "month": ["01", "06"],
-    "year": [
-        "2000", "2001", "2002",
-        "2003", "2004", "2005",
-        "2006", "2007", "2008",
-        "2009", "2010", "2011",
-        "2012", "2013", "2014"
-    ],
-    "area": [68.5, -27, 61.5, -10],  # Zone : Nord, Ouest, Sud, Est
-}
 
 with fs.open(minio_path, "wb") as minio_file:  # Fichier cible sur MinIO
     with open(file_path, "rb") as local_file:  # Fichier source local
@@ -118,6 +101,8 @@ df_cds_clean.to_csv("projet_python_2024_ENSAE/sst_data_cds.csv", index=False)
 
 
 
+
+
 #### Climate Data Storage Copernicus 2 ####
 # Model HadGEM3-GC31-LL (UK) - from 2015 to 2049 - SST - SSP5-8.5 (worst scenario)
 
@@ -150,11 +135,52 @@ client.retrieve(dataset, request).download()
 
 local_zip_path_2 = "/home/onyxia/work/cds_output_2.zip"
 temp_dir = "./extracted_files" # Dossier temporaire pour extraire les fichiers
-with zipfile.ZipFile(local_zip_path, 'r') as zip_ref: # Ouvrez le fichier ZIP et extrayez son contenu
+with zipfile.ZipFile(local_zip_path_2, 'r') as zip_ref: # Ouvrez le fichier ZIP et extrayez son contenu
     zip_ref.extractall(temp_dir)
 print(f"Fichiers extraits dans : {temp_dir}")
 
 # Charger la liste des fichiers extraits dans un DataFrame
-extracted_files_cds = pd.DataFrame({"file_paths": zip_ref.namelist()})
+extracted_files_cds_2 = pd.DataFrame({"file_paths": zip_ref.namelist()})
 print("Fichiers extraits :")
-print(extracted_files_cds)
+print(extracted_files_cds_2)
+
+
+
+# Initialiser MiniO
+fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
+MY_BUCKET = "esam"
+print(fs.ls(MY_BUCKET))
+minio_path_2 = f'{MY_BUCKET}/diffusion/cds_data/tos_Omon_HadGEM3-GC31-LL_ssp585_r1i1p1f3_gn_20150116-20490616.nc'
+file_path_2 = "/home/onyxia/work/extracted_files/tos_Omon_HadGEM3-GC31-LL_ssp585_r1i1p1f3_gn_20150116-20490616.nc"
+
+
+with fs.open(minio_path_2, "wb") as minio_file:  # Fichier cible sur MinIO
+    with open(file_path_2, "rb") as local_file:  # Fichier source local
+        minio_file.write(local_file.read())
+
+print(fs.ls(f"{MY_BUCKET}/diffusion/cds_data"))
+
+
+
+# Exploration des données
+fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
+MY_BUCKET = "esam"
+minio_path_2 = f'{MY_BUCKET}/diffusion/cds_data/tos_Omon_HadGEM3-GC31-LL_ssp585_r1i1p1f3_gn_20150116-20490616.nc'
+local_file_2 = "temp_file_2.nc"
+
+with fs.open(minio_path_2, "rb") as minio_file:
+    with open(local_file_2, "wb") as local_nc_file:
+        local_nc_file.write(minio_file.read())
+
+# Charger le fichier localement avec xarray
+dataset = xr.open_dataset(local_file_2, engine="netcdf4")
+print(dataset)
+
+df_cds_2 = dataset.to_dataframe().reset_index()
+print(df_cds_2.head())
+
+df_cds_clean = df_cds_2.dropna(subset=["tos"])
+print(df_cds_clean.head())
+
+df_cds_clean.to_csv("projet_python_2024_ENSAE/sst_data_cds_SSP5.csv", index=False)
+
