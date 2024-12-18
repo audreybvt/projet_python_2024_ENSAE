bab7262b12ffc316946a03fb199866d5744648bf
abovet <audrey.bovet@ensae.fr>
2024-12-18 18:07:55 +0000

    je récupère la variable de sst

--- visuamiser des données nc.py
+++ visuamiser des données nc.py
@@ -10,13 +10,25 @@ data = xr.open_dataset(file_path)
 print(data)  # Résumé des dimensions, variables et attributs
 print(data.variables)  # Liste des variables disponibles
 
-'''
+
 # Accéder à une variable spécifique
-print(data['nom_de_la_variable'])  # Remplacer par une variable réelle
+print(data['sea_surface_temperature'])  # Remplacer par une variable réelle
 
 # Extraire les données d'une variable en tant que tableau NumPy
-variable_data = data['nom_de_la_variable'].values
+variable_data = data['sea_surface_temperature'].values
 
 # Fermer le fichier pour libérer les ressources
 data.close()
-'''
+
+import matplotlib.pyplot as plt
+
+# Exemple : Lire et visualiser la température de surface
+temperature = data['sea_surface_temperature']  # Remplacer 'tos' par le nom réel de la variable
+
+# Sélectionner une tranche temporelle spécifique (si pertinent)
+temperature_at_time = temperature.isel(time=0)  # Première étape temporelle
+
+# Tracer la variable
+temperature_at_time.plot()
+plt.title("Température de surface des océans (temps=0)")
+plt.show()
