0947396576d3a55c36df6e149bb2a7984be80da7
esam <eve.samani@ensae.fr>
2024-12-23 12:46:39 +0000

    pbl API

--- data/explorationGPS.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import xarray as xr
-
-
-df_1 = pd.read_csv('/home/onyxia/work/projet_python_2024_ENSAE/data/Dataset_15338_1072_Atlantic_Puffin_from_Papey_GL_imported.csv')
-print(df_1.head(50))
